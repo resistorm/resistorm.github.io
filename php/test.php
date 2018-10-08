8650136dd2d1b3ec8870a644e780da29e5b24a02
resistorm <43294500+resistorm@users.noreply.github.com>
2018-10-08 15:28:38 +0200

    Delete test.php

--- php/test.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-        <body>
-                <h1>Testing PHP</h1>
-                <?php
-                echo "Hello World!";
-                ?>
-        </body>
-</html>
